238f43eeba32c0b87983746b108de6c0e2ced943
Chetn11 <cnpatil00@gmail.com>
2024-03-20 19:26:11 +0530

    basic structure

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import WorkflowBuilder from './components/WorkflowBuilder';
+
+function App() {
+  return (
+    <div className="App">
+      <WorkflowBuilder/>
+    </div>
+  );
+}
+
+export default App;
